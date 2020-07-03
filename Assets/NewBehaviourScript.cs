d61b8097de148387c25c4b6afc3909d1b0faf186
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-07-03 08:30:24 +0800

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+   public void Move()
+    {
+        GetComponent<Rigidbody>().AddForce(4, 4, 0);
+
+
+    }
+
+
+
+}
---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool isOpen;
+    public bool isAct;
+
+    public void Open()
+    {
+        if (!isOpen)
+        {
+
+            if (transform.eulerAngles.y < 89)
+            {
+                transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+            }
+
+            if (transform.eulerAngles.y > 89)
+            {
+                transform.eulerAngles = Vector3.up * 90;
+                isOpen = true;
+                isAct = false;
+            }
+
+        }
+
+        else
+        {
+            if (transform.eulerAngles.y > 1 && isOpen)
+            {
+                transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime);
+            }
+
+            if ((transform.eulerAngles.y < 1 || transform.eulerAngles.y > 345) && isOpen)
+            {
+                transform.eulerAngles = Vector3.zero;
+                isOpen = false;
+                isAct = false;
+            }
+        }
+    }
+   
+
+    public void trigger()
+    {
+        isAct = true;
+    }
+    
+    private void Update()
+    {  
+        if (isAct)
+        {
+        Open();
+        }
+       
+      
+    }
+
+
+}
